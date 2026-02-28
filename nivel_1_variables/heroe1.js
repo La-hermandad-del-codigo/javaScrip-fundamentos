89b27f9ff2cc4deb7ece0c52dc37aa78293808c0
Gustavo Tuesta <gustavo.tuesta.puse@gmail.com>
2026-02-28 12:28:24 -0500

    Contando historia heroe 1

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -3,3 +3,6 @@ let poder = 89;
 let nivelEnergia = 50;
 let ciudad = "Tokyo"
 let enemigo = "All for one"
+
+console.log(`${nombreHeroe} tiene ${poder} de poder, pero tiene ${nivelEnergia} de energia y se encuentra peleando con ${enemigo} en ${ciudad}`);
+
