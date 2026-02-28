1f2ffbb5d5eec762bf5da629b655418580956dd6
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 12:36:07 -0500

    creacion de historia de el heroe 2

--- nivel_1_variables/herore2.js
+++ nivel_1_variables/herore2.js
@@ -3,3 +3,5 @@ let poder = 85;
 let nivelEnergia = 98;
 let ciudad = "Ende"
 let enemigo = "Aura"
+
+console.log(`${nombreHeroe} tiene ${poder} de poder,se encuentra peleando con ${enemigo} en ${ciudad} y ya solo le queda ${nivelEnergia - 50} de energia`);
