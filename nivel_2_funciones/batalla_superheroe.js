566c125df654b95103d1f7f6bd8332b241cd71a8
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 12:54:48 -0500

    creacion de funcion ataque

--- nivel_2_funciones/batalla_superheroe.js
+++ nivel_2_funciones/batalla_superheroe.js
@@ -9,3 +9,8 @@ let defensaHeroe2 = 3;
 let energiaActual = 0;
 let nombreAtaque = "PLUS ULTRA";
 let curacion = 0;
+
+function ataque(nombre1, nombre2, ataque, energia, recuperacion) {
+    console.log(`${nombre2} ataco a ${nombre1}`)
+    defender(nombre1, nombre2, ataque, energia, recuperacion)
+}
