5b6b456405cf4c3e30776e2d01e7f6237f73b5a7
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 13:06:50 -0500

    creacion de la funcion ordenar comida

--- nivel_3_objetos/restaurante.js
+++ nivel_3_objetos/restaurante.js
@@ -4,3 +4,10 @@ let cliente = {
     hambre: 60
 }
 
+
+function ordenarComida(cliente, comida) {
+    console.log(`${cliente.nombre} pidio un(a) ${comida.nombre} el cual cuesta ${comida.precio} soles.`)
+    cliente.hambre -= comida.satisfaccion;
+    pagar(cliente, comida);
+
+}
