ba15a1599c1bf831a0573d5b83843003a5937d62
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 12:57:11 -0500

    creacion de la funcion escudo magico

--- nivel_2_funciones/batalla_superheroe.js
+++ nivel_2_funciones/batalla_superheroe.js
@@ -20,3 +20,9 @@ function defender(nombre1, nombre2, ataque, energia, recuperacion) {
     console.log(`${nombre1} recibio ${ataque} de daño por parte de ${nombre2}, le queda ${energiaActual} puntos de energia`)
     recargarEnergia(nombre1, energiaActual, recuperacion)
 }
+
+
+function escudoMagico(nom1, nom2) {
+    console.log(`${nom2} uso su escudo magico para anular el super ataque de ${nom1}`)
+
+}
