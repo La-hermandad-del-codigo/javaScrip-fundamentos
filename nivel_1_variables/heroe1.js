5a5dfcd68b705a8880561f2d02d4868f17783d3a
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 12:42:38 -0500

    extension de la historia y uso de la variable

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -7,3 +7,5 @@ let enemigo = "All for one"
 console.log(`${nombreHeroe} tiene ${poder} de poder, pero tiene ${nivelEnergia} de energia y se encuentra peleando con ${enemigo} en ${ciudad}`);
 
 nivelEnergia = 30;
+
+console.log(`${nombreHeroe} se canso por pelear mucho y su energia bajo a ${nivelEnergia}`)
