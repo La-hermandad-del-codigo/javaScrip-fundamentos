62f9a9da95f84808a131a2d9d48078f46199cb7c
Gustavo Tuesta <gustavo.tuesta.puse@gmail.com>
2026-02-28 16:10:25 -0500

    Integracion funcion de lesion

--- nivel_4_objetos/jugadores.js
+++ nivel_4_objetos/jugadores.js
@@ -8,7 +8,7 @@ let jugadores = {
 
     jugador2: {
         nombre: "Gustavo",
-        vida: 70,
+        vida: 60,
         fuerza: 60,
         nivel: 19
     }
@@ -19,4 +19,12 @@ function entrenamiento(jugador) {
     aumentoFuerza = Math.floor(Math.random() * 5) + 1;
     jugador.fuerza += aumentoFuerza;
     console.log(`El jugador ${jugador.nombre} aumento su fuerza en ${aumentoFuerza}, ahora tiene ${jugador.fuerza} en total y su vida aumento en 1, ahora tiene ${jugador.vida} en total`);
+}
+
+function lesion(jugador) {
+    disminucionVida = Math.floor(Math.random() * 15) + 1;
+    disminucionFuerza = Math.floor(Math.random() * 10) + 1;
+    jugador.vida -= disminucionVida;
+    jugador.fuerza -= disminucionFuerza;
+    console.log(`El jugador ${jugador.nombre} ha sufrido una lesión, lo que disminuyo su vida en ${disminucionVida} y ahora tiene ${jugador.vida}, tambien su fuerza disminuyo en ${disminucionFuerza} y ahora tiene ${jugador.fuerza}`)
 }
