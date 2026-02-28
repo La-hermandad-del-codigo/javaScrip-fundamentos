6fac5bbfc8eacebd37c539828294486987700067
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 12:56:44 -0500

    creacion de la funcion defender

--- nivel_2_funciones/batalla_superheroe.js
+++ nivel_2_funciones/batalla_superheroe.js
@@ -14,3 +14,9 @@ function ataque(nombre1, nombre2, ataque, energia, recuperacion) {
     console.log(`${nombre2} ataco a ${nombre1}`)
     defender(nombre1, nombre2, ataque, energia, recuperacion)
 }
+
+function defender(nombre1, nombre2, ataque, energia, recuperacion) {
+    energiaActual = energia - ataque
+    console.log(`${nombre1} recibio ${ataque} de daño por parte de ${nombre2}, le queda ${energiaActual} puntos de energia`)
+    recargarEnergia(nombre1, energiaActual, recuperacion)
+}
