f59fd820b9184bf2a8d4cdf92220c0846f1a60b3
Gustavo Tuesta <gustavo.tuesta.puse@gmail.com>
2026-02-28 15:58:23 -0500

    Creacion del jugador 2

--- nivel_4_objetos/jugadores.js
+++ nivel_4_objetos/jugadores.js
@@ -1,15 +1,22 @@
 let jugadores = {
-    jugador1 :{
+    jugador1: {
         nombre: "jhefry",
         vida: 70,
         fuerza: 80,
         nivel: 22
+    },
+
+    jugador2: {
+        nombre: "Gustavo",
+        vida: 70,
+        fuerza: 60,
+        nivel: 19
     }
 }
 
 function entrenamiento(jugador) {
     jugador.vida += 1;
-    aumentoFuerza = Math.floor(Math.random()*5)+1;
+    aumentoFuerza = Math.floor(Math.random() * 5) + 1;
     jugador.fuerza += aumentoFuerza;
     console.log(`El jugador ${jugador.nombre} aumento su fuerza en ${aumentoFuerza}, ahora tiene ${jugador.fuerza} en total y su vida aumento en 1, ahora tiene ${jugador.vida} en total`);
 }
